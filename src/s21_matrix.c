25bc3c1d496c071ed87e4d0092d3a1aa4d3d4e73
bebe-pirat <pirat.bebe@gmail.com>
2025-07-26 09:30:22 +0300

    Update s21_matrix.c, added s21_eq_matrix function

--- src/s21_matrix.c
+++ src/s21_matrix.c
@@ -47,3 +47,20 @@ void s21_remove_matrix(matrix_t *A) {
         A->columns = 0;
     }
 }
+
+int s21_eq_matrix(matrix_t *A, matrix_t *B) {
+    int result = SUCCESS;
+    if (A == NULL || B == NULL) result = FAILURE;
+    else {
+        if (A->rows != B->rows || A->columns != B->columns) result = FAILURE;
+        else {
+            for (int i = 0; i < A->rows; i++) {
+                for (int j = 0; j < A->columns; j++) {
+                    if (A->matrix[i][j] != B->matrix[i][j]) result = FAILURE;
+                }
+            }
+        }
+    } 
+    
+    return result;
+}
